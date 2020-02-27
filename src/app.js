5d4023363aabb99ed643bda287fb40de34e444ff
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2020-02-27 10:45:56 -0500

    modified files and added logging

--- src/app.js
+++ src/app.js
@@ -4,22 +4,68 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const logger = require('./logger')
 
 const app = express()
-
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common'
 
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(bodyParser.json())
 
-//Home endpoint
+
+//DB CONNECTION
+mongoose.connect(
+    process.env.MONGODB_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connection to DB established.")
+    })
+    .catch(error => {
+        connectionErrorHandler(error)
+    })
+
+mongoose.connection.on('error', error => connectionErrorHandler(error))
+
+function connectionErrorHandler(error) {
+    console.log('Error: ', error.message)
+}
+
+
+
+//SECURITY HANDLE MIDDLEWARE
+function handleAccessSecurity(req, res, next) {
+    const apiKey = req.get('authorization')
+
+    //avoid apiKey on home ('/') or about ('/about') paths
+    if(req.path === '/' || req.path === '/about') {
+        next()
+    }
+    else if (!apiKey || apiKey.split(' ')[1] !== process.env.API_KEY) { //mandatory apiKey for the rest of the paths
+        logger.error(`Unauthorized request to path: ${req.path}`)
+        next({ status: '401', message: 'Unauthorized request' })
+    }
+
+    next()
+}
+app.use(handleAccessSecurity)
+
+
+
+//HOME ENDPOINT
 app.route('/')
     .get((req, res) => {
         res.status(200).json('Template Project')
     })
+
+//MODEL ENDPOINT
+//app.route(modelRouter)
+
+
 
 //Error Handling middleware
 function errorHandler(error, req, res, next) {
@@ -38,5 +84,6 @@ function errorHandler(error, req, res, next) {
     res.status(response.error.status).json(response);
 }
 app.use(errorHandler)
+
 
 module.exports = app

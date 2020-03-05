bf2643b5eb2205084b6d4f38d163300082a5cca7
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2020-03-05 10:25:09 -0500

    moved accessHandler and errorHandler out of app.js

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const bodyParser = require('body-parser')
+const accessHandler = require('./access-handler')
 const { NODE_ENV } = require('./config')
 const logger = require('./logger')
+const errorHandler = require('./error-handler')
 
 const app = express()
 const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common'
@@ -15,23 +17,8 @@ app.use(helmet())
 app.use(cors())
 app.use(bodyParser.json())
 
-
 //SECURITY HANDLE MIDDLEWARE
-function handleAccessSecurity(req, res, next) {
-    const apiKey = req.get('authorization')
-
-    //avoid apiKey on home ('/') or about ('/about') paths
-    if(req.path === '/' || req.path === '/about') {
-        next()
-    }
-    else if (!apiKey || apiKey.split(' ')[1] !== process.env.API_KEY) { //mandatory apiKey for the rest of the paths
-        logger.error(`Unauthorized request to path: ${req.path}`)
-        next({ status: '401', message: 'Unauthorized request' })
-    }
-
-    next()
-}
-app.use(handleAccessSecurity)
+app.use(accessHandler)
 
 
 
@@ -46,22 +33,6 @@ app.route('/')
 
 
 
-//Error Handling middleware
-function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = {
-            error: { message: "server error", status: 500 }
-        }
-    }
-    else {
-        response = {
-            error: { message: error.message, status: error.status ? error.status: 500 }
-        }
-    }
-
-    res.status(response.error.status).json(response);
-}
 app.use(errorHandler)
 
 
